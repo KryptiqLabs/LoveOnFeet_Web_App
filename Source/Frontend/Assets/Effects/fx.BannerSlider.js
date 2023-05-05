2e235a60d59e465c3a012d6ed40c3b2436dda25f
Kryptiq Labs - Research & Development <kryptiqlabs.rd@gmail.com>
2023-05-05 06:22:59 +0200

    Updates to Slideshow, banner-images & theme colour-accents.

--- Source/Frontend/Assets/Effects/fx.BannerSlider.js
+++ Source/Frontend/Assets/Effects/fx.BannerSlider.js
@@ -6,8 +6,7 @@
 
 
 
-var myIndex = 0;
-carousel();
+var myIndex1 = 0;
 
 function carousel() {
   var i;
@@ -15,8 +14,38 @@ function carousel() {
   for (i = 0; i < x.length; i++) {
     x[i].style.display = "none";  
   }
-  myIndex++;
-  if (myIndex > x.length) {myIndex = 1}    
-  x[myIndex-1].style.display = "block";  
+  myIndex1++;
+  if (myIndex1 > x.length) {myIndex1 = 1}    
+  x[myIndex1-1].style.display = "block";  
   setTimeout(carousel, 5000); // Change image every 5 seconds
 }
+
+
+
+carousel();
+
+
+
+
+
+
+
+var myIndex2 = 0;
+
+function carouselMobile() {
+  var i;
+  var x = document.getElementsByClassName("bannerSliderMobile");
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";  
+  }
+  myIndex2++;
+  if (myIndex2 > x.length) {myIndex2 = 1}    
+  x[myIndex2-1].style.display = "block";  
+  setTimeout(carouselMobile, 5000); // Change image every 5 seconds
+}
+
+
+
+carouselMobile();
+
+
